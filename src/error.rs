5e3d71006bb2925567d2ec6d5c3068c3ee5455db
Arnaud Jungers <arnaud.jungers@gmail.com>
2023-04-20 17:49:45 +0200

    refactor: refactor the code

--- src/error.rs
+++ src/error.rs
@@ -4,10 +4,10 @@ pub enum Error {
     Generic(String),
 
     #[error("Database creation error: {url}, {source}")]
-    DatabaseCreationError {url: String, source: sqlx::Error},
+    DatabaseCreationError { url: String, source: sqlx::Error },
 
     #[error("Database migration error: {source}")]
-    DatabaseMigrationError {source: sqlx::migrate::MigrateError},
+    DatabaseMigrationError { source: sqlx::migrate::MigrateError },
 
     #[error("Database connection error: {url}, {source}")]
     DatabaseConnectionError { url: String, source: sqlx::Error },
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use WorkoutTrack::prelude::*;
-use WorkoutTrack::utils::database::create_and_migrate_database;
+use workout_track::prelude::*;
+use workout_track::utils::database::create_and_migrate_database;
 
 const DB_URL: &str = "sqlite://sqlite.db";
-
 
 #[tokio::main]
 async fn main() -> Result<()> {
