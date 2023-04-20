224949c386a2985041a59ad90ff090872a317cd9
Arnaud Jungers <arnaud.jungers@gmail.com>
2023-04-20 12:24:20 +0200

    feat(database): :card_file_box: Add database migrations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{SqlitePool};
 use WorkoutTrack::prelude::*;
 use WorkoutTrack::utils::database::create_database;
 
@@ -8,6 +9,19 @@ const DB_URL: &str = "sqlite://sqlite.db";
 async fn main() -> Result<()> {
     // Create the database if it doesn't exist
     create_database(DB_URL).await?;
+    println!("Database created!");
+
+    // Connect to the database
+    let db = SqlitePool::connect(DB_URL).await.unwrap();
+    println!("Database connected!");
+
+    // Migrate the database
+    sqlx::migrate!("./migrations")
+        .run(&db)
+        .await
+        .or_else(|err| return Err(Error::DatabaseMigrationError {source: err}))?;
+
+    println!("Migration successful!");
 
     Ok(())
 }
