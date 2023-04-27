06cea8e6e3de365db9b7ffc43b5f532db7fe870a
Arnaud Jungers <arnaud.jungers@gmail.com>
2023-04-27 19:45:38 +0200

    feat(frontend): add key to workout list

--- src/components/molecules/workout.rs
+++ src/components/molecules/workout.rs
@@ -20,12 +20,12 @@ pub fn workout(props: &WorkoutProps) -> Html {
 
     let date = work_date.format("%B %e, %Y").to_string();
     html! {
-        <div class="bg-white rounded-lg shadow-md p-6 flex flex-row justify-between items-center mx-2">
+        <div key={uuid.to_string()} class="bg-white rounded-lg shadow-md p-6 flex flex-row justify-between items-center mx-2">
             <div class="flex flex-col">
                 <div class="text-gray-600 font-bold text-lg mb-2 text-left">{title}</div>
                 <div class="text-gray-400 text-sm text-left">{date}</div>
             </div>
-            <div class="text-green-500 font-bold text-3xl">{"$99"}</div>
+            <div class="text-green-500 font-bold text-3xl">{id}</div>
         </div>
     }
 }
